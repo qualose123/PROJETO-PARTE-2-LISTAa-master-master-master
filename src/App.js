06bf6c592a54635f847e60e8933541f7b97b5bdb
qualose123 <jonhyzicaos@gmail.com>
2022-07-19 21:51:54 -0300

    fzr o edit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './Views/Home/Home'
 import Details from './Views/Details/Details';
+import Edit from './Views/Edit/Edit';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>TO-DOO-LIST</h1>
     
        <Routes>
+        <Route path="/edit/:id" element={<Edit/>}/>
         <Route path="/" element={<Home />}/>
         <Route path="/detalhes/:id" element={<Details />}/>
       </Routes>
---
+++ src/Views/Edit/Edit.jsx
@@ -0,0 +1,12 @@
+import './Edit.css'
+import { useState,useEffect } from "react";
+import {Link, useParams } from 'react-router-dom'
+
+function Edit (){
+
+    return(
+        <>
+        </>
+    )
+}
+export default Edit
